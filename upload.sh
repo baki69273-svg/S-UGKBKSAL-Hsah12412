fc66e98ff2cc8f7f137909a39f5ffd5f6a33f5e9
baki69273-svg <baki69273@gmail.com>
2026-03-22 14:56:32 +0700

    upload 1774166192_chrome_H6mca01cO5.png

---
+++ upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vào đúng repo
+cd /d/GitHub_SLH/S-UGKBKSAL-Hsah12412
+img="$1"
+# kiểm tra file
+[ ! -f "$img" ] && echo "File not found!" && exit 1
+# tạo tên file mới với timestamp
+filename=$(date +%s)_$(basename "$img")
+mkdir -p images
+cp "$img" "images/$filename"
+# commit & push
+git add .
+git commit -m "upload $filename"
+git pull --rebase origin main
+git push
+# tạo URL
+url="https://raw.githubusercontent.com/baki69273-svg/S-UGKBKSAL-Hsah12412/main/images/$filename"
+# copy vào clipboard
+echo "$url" | /c/Windows/System32/clip.exe
+# hiển thị URL
+echo "$url"
